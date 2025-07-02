37f87e2995d79b31d7cb4e4442a78b72df082816
Провальская Дарья <provalskayadaria@gmail.com>
2025-07-02 17:50:53 +0300

    feat(header): add header layout and extract style variables

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <div className={styles["logo-container"]}>
+      <p>Modsen Calendar</p>
+    </div>
+  );
+};
